0f934699c3f451a599ae36e4e4e4ee0ecc0783d4
Mathieu Colla <mathieucolla@gmail.com>
2025-03-26 17:00:02 +0100

    create shared folder and implement services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GithubService } from './shared/services/github.service';
+import { CodeLanguagesService } from './shared/services/code-languages.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [
+    GithubService,
+    CodeLanguagesService
+  ],
   template: '<router-outlet />',
 })
 export class AppComponent {}

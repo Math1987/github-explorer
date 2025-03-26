a189ddbe66762d32454357d2e1d209b0269c0618
Mathieu Colla <mathieucolla@gmail.com>
2025-03-26 17:34:44 +0100

    clean not necessary selector in repositories component

--- src/app/features/repositories/repositories.component.ts
+++ src/app/features/repositories/repositories.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-repositories',
   templateUrl: './repositories.component.html',
   styleUrl: './repositories.component.scss',
   standalone: false

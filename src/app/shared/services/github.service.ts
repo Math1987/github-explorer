b63124d6f8b5b386c89c76bdd4a8a6e354f1c536
Mathieu Colla <mathieucolla@gmail.com>
2025-03-26 17:28:20 +0100

    add request to get repositories on github api and fix http configuration

--- src/app/shared/services/github.service.ts
+++ src/app/shared/services/github.service.ts
@@ -1,10 +1,38 @@
+import { environment } from "@/environments/environment";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { RepositoriesDto } from "@/app/shared/models/repositories.dto.model";
 
+const PER_PAGE_DEFAULT = 10;
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Search for repositories based on query parameters
+   * @param query Search query string
+   * @param page Page number
+   * @param perPage Number of results per page
+   */
+  searchRepositories(
+    query: string,
+    page: number = 0,
+    perPage: number = PER_PAGE_DEFAULT
+  ): Observable<RepositoriesDto> {
+
+    const params = new HttpParams()
+      .set('q', query)
+      .set('page', (page + 1).toString())
+      .set('per_page', perPage.toString());
+
+    return this.http.get<RepositoriesDto>(
+      `${environment.gitHubApiUrl}/search/repositories`,
+      { params }
+    );
+  }
 
 }

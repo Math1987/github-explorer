23b29d3552e1ad2e2a005807d7b3f64d364bb8fa
Mathieu Colla <mathieucolla@gmail.com>
2025-03-26 16:33:29 +0100

    add structure of routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'repos',
+        loadChildren: () => import('./features/repositories/repositories.module').then(m => m.RepositoriesModule),
+      },
+      {
+        path: 'commits',
+        loadChildren: () => import('./features/commits/commits.module').then(m => m.CommitsModule),
+      },
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'repos',
+    pathMatch: 'full'
+  }
+
+];
